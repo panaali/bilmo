6ed809fd24f737bb2998e09ad230d4a52fbcce6e
Ali Panahi <panaali@live.com>
2019-08-01 02:55:14 -0400

    disable lm fp16

--- v4/lm.py
+++ v4/lm.py
@@ -18,6 +18,8 @@ import os
 from torch import nn
 
 data_path = './data/'
+bs = 256
+fp16 = False
 
 class dna_tokenizer(BaseTokenizer):
     def tokenizer(self, t):
@@ -67,14 +69,13 @@ def main(gpu: Param("GPU to run on", str) = None):
     df = pickle.load(
         open('./data/sprot_lm/sproat_sequence_taxon_anc.pickle', 'rb'))
 
-    bs = 512
     data_lm = (TextList.from_df(df, path=local_project_path, cols='seq_anc_tax', processor=processor)
                     .split_by_rand_pct(0.1, seed = random_seed)
                     .label_for_lm()
                     .databunch(bs=bs, num_workers=workers))
 
-    data_lm.vocab.save(local_project_path +
-                       'vocab_lm_sproat_seq_anc_tax.pickle')
+    # data_lm.vocab.save(local_project_path +
+    #                    'vocab_lm_sproat_seq_anc_tax.pickle')
 
     print('data_cls Training set size', len(data_lm.train_ds))
     print('data_cls Validation set size', len(data_lm.valid_ds))
@@ -82,7 +83,7 @@ def main(gpu: Param("GPU to run on", str) = None):
 
 
     learn_lm = language_model_learner(
-        data_lm, AWD_LSTM, drop_mult=1, pretrained=False)
+        data_lm, AWD_LSTM, drop_mult=0.1, pretrained=False)
 
     if gpu is None:
         # print(gpu, 'DataParallel')
@@ -91,34 +92,28 @@ def main(gpu: Param("GPU to run on", str) = None):
     else:
         print(gpu, 'to_distributed')
         learn_lm.to_distributed(gpu)
-        learn_lm.to_fp16()
+        if fp16:
+            learn_lm.to_fp16()
     
 
     lr = 3e-3
     print(gpu, 'freeze')
     learn_lm.freeze()
     learn_lm.fit_one_cycle(1, lr, moms=(0.8, 0.7))
-    # print(gpu, 'unfreeze')
-    # learn_lm.unfreeze()
-    # learn_lm.fit_one_cycle(1, lr, moms=(0.8, 0.7))
 
+    print(gpu, 'unfreeze')
+    learn_lm.unfreeze()
+    learn_lm.fit_one_cycle(10, lr*10, moms=(0.8, 0.7))
+    learn_lm.save('lm-sp-anc-v1-1')
+    learn_lm.save_encoder('lm-sp-ans-v1-1-enc')
+    
+    learn_lm.fit_one_cycle(10, lr, moms=(0.8, 0.7))
+    learn_lm.save('lm-sp-anc-v1-2')
+    learn_lm.save_encoder('lm-sp-ans-v1-2-enc')
 
-    # learn_lm.load('lm-gpu3-sp-40M-v2');
-
-    # learn_lm.freeze()
-    # lr = 3e-3
-    # learn_lm.unfreeze()
-    # learn_lm.fit_one_cycle(1, lr, moms=(0.8, 0.7))
-    # learn_lm.save('lm-whole-sp-dist-v1')
-    # learn_lm.save_encoder('lm-whole-sp-dist-v1-enc')
-
-    # learn_lm.fit_one_cycle(10, lr/10, moms=(0.8, 0.7))
-    # learn_lm.save('lm-whole-sp-dist-v2')
-    # learn_lm.save_encoder('lm-whole-sp-dist-v2-enc')
-
-    # learn_lm.fit_one_cycle(10, slice(lr/2.6**4, lr/100), moms=(0.8, 0.7))
-    # learn_lm.save('lm-whole-sp-dist-v3')
-    # learn_lm.save_encoder('lm-whole-sp-dist-v2-enc')
+    learn_lm.fit_one_cycle(10, lr/10, moms=(0.8, 0.7))
+    learn_lm.save('lm-sp-anc-v1-3')
+    learn_lm.save_encoder('lm-sp-ans-v1-3-enc')
     print('Done')
 
 # main(None)
