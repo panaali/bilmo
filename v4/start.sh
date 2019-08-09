530517a9fd865df0eeabd6485ae777fa3d18ba74
Aliakbar Panahi <panaali@gmail.com>
2019-08-08 23:32:28 -0400

    added start.sh

---
+++ v4/start.sh
@@ -0,0 +1,13 @@
+python -m fastai.launch \
+--gpus=01 v4/cls.py "./data/cafa3/CAFA 3 Protein Targets/CAFA3_training_data/cafa_train_enhanced.p" \
+--max_cpu_per_dataloader=40 \
+--bs=32 \
+--fp16=0 \
+--use_sp_processor=1  \
+--sp_model=./data/sprot_lm/tmp/spm.model \
+--sp_vocab=./data/sprot_lm/tmp/spm.vocab \
+--lm_encoder lm-sp-ans-v1-3-enc2019-08-01_18-17-18  \
+--sequence_col_name seq_anc_tax \
+--label_col_name selected_go \
+--selected_go GO:0017076 \
+--benchmarking 0 \
