5195bddb6939294e17e9e9e1d65e49f6656adada
Ali Panahi <panaali@live.com>
2019-08-12 07:54:21 -0400

    fixed killer

--- src/callbacks/killer.py
+++ src/callbacks/killer.py
@@ -1,12 +1,12 @@
 import os
 import torch
-from fastai.callback import Callback
+from fastai.basic_train import LearnerCallback
 from fastai.torch_core import *
 
 __all__ = ['KillerCallback']
 
-class KillerCallback(Callback):
-    def check_for_killme():
+class KillerCallback(LearnerCallback):
+    def check_for_killme(self, **kwargs: Any) -> None:
         if os.path.isfile('kill.me'):
             num_gpus = torch.cuda.device_count()
             for gpu_id in range(num_gpus):
@@ -17,8 +17,5 @@ class KillerCallback(Callback):
                     pass
             exit(0)
 
-    def on_batch_begin(self, **kwargs: Any) -> None:
-        check_for_killme()
-
-    def on_batch_end(self, **kwargs: Any) -> None:
-        check_for_killme()
+    def on_epoch_end(self, **kwargs: Any) -> None:
+        self.check_for_killme()
